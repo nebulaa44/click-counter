bcd23e4ed5d868b029a79b518bccee655a07f4b6
kai <gmdezreal@gmail.com>
2022-07-17 15:59:49 -0500

    add the actual gui

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
+// meson config file
+#include <config.h>
 #include <gtk/gtk.h>
 
-int main() {
-  printf("Hello world!\n");
+#include "window.h"
+
+int main(int argc, char** argv) 
+{
+  GtkApplication *app = gtk_application_new("com.github.nebulaa44.ClickCounter", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+
+  int status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+
+  return status;
 }
